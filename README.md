2baf5751bdc7251711d847bce1ed9ad21326bf66
cibo-24 <cibo.kenan@gmail.com>
2024-01-15 23:55:48 +0300

    finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Responsive-Picture-Cards-Website
 I made this project using html, css and javascript technologies 💎 📐
+
+<img src="./images/project.png" alt="img">
+
+# Author: cibo-24
---
+++ app.js
@@ -0,0 +1,49 @@
+
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let cardBorderDom = document.querySelector('.carousel .thumbnail');
+let cardItemsDom = cardBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+cardBorderDom.appendChild(cardItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let  cardItemsDom = document.querySelectorAll('.carousel .card .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        cardBorderDom.appendChild(cardItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        cardBorderDom.prepend(cardItemsDom[cardItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
